a0905d9d82732d9ec9906fec66054d6324523803
Huguie <Hyj2862588507@163.com>
2025-02-21 23:36:33 -0800

    add CH_KEF_CZK_2004

---
+++ scheme/CH/CH_KEF_CZK_2004/PBC.go
@@ -0,0 +1,103 @@
+package CH_KEF_CZK_2004
+
+import (
+	"github.com/Hugu1e/ChameLibGo/curve"
+	"github.com/Hugu1e/ChameLibGo/utils"
+	"github.com/Nik-U/pbc"
+)
+
+type PublicParam struct {
+	Group pbc.Field
+	Pairing *pbc.Pairing
+
+	G pbc.Element
+}
+func (pp *PublicParam) GetGroupElement() *pbc.Element {
+	switch pp.Group {
+	case pbc.G1:
+		return pp.Pairing.NewG1().Rand()
+	case pbc.G2:
+		return pp.Pairing.NewG2().Rand()
+	case pbc.GT:
+		return pp.Pairing.NewGT().Rand()
+	default:
+		return nil
+	}
+}
+func (pp *PublicParam) GetZrElement() *pbc.Element {
+	return pp.Pairing.NewZr().Rand()
+}
+func (pp *PublicParam) H(m string) *pbc.Element {
+	return utils.H_String_1_PBC_1(pp.Pairing, pp.Group, m)
+}
+
+type PublicKey struct {
+	Y pbc.Element
+}
+
+type SecretKey struct {
+	X pbc.Element
+}
+
+type HashValue struct {
+	H pbc.Element
+}
+
+type Randomness struct {
+	G_a pbc.Element
+	Y_a pbc.Element
+}
+
+
+func getHashValue(R *Randomness, SP *PublicParam, I, m *pbc.Element) *pbc.Element {
+	return utils.MUL(&SP.G, I).ThenPowZn(m).ThenMul(&R.Y_a)
+}
+
+
+func SetUp(curveName curve.Curve, group pbc.Field) *PublicParam {
+	pp := new(PublicParam)
+
+	pp.Pairing = curve.PairingGen(curveName)
+	pp.Group = group
+
+	pp.G = *pp.GetGroupElement()
+
+	return pp
+}
+
+func KeyGen(pp *PublicParam) (*PublicKey, *SecretKey) {
+	pk := new(PublicKey)
+	sk := new(SecretKey)
+
+	sk.X = *pp.GetZrElement()
+	pk.Y = *utils.POWZN(&pp.G, &sk.X)
+
+	return pk, sk
+}
+
+func Hash(pp *PublicParam, pk *PublicKey, I, m *pbc.Element) (*HashValue, *Randomness) {
+	H := new(HashValue)
+	R := new(Randomness)
+
+	a := pp.GetZrElement()
+	R.G_a = *utils.POWZN(&pp.G, a)
+	R.Y_a = *utils.POWZN(&pk.Y, a)
+	H.H = *getHashValue(R, pp, I, m)
+
+	return H, R
+}
+
+func Check(H *HashValue, R *Randomness, SP *PublicParam, I, m *pbc.Element) bool {
+	return H.H.Equals(getHashValue(R, SP, I, m))
+}
+
+func Adapt(R *Randomness, SP *PublicParam, sk *SecretKey, I, m, m_p *pbc.Element) *Randomness {
+	R_p := new(Randomness)
+
+	gI := utils.MUL(&SP.G, I)
+	delta_m := utils.SUB(m ,m_p)
+	R_p.Y_a = *utils.POWZN(gI, delta_m).ThenMul(&R.Y_a)
+	R_p.G_a = *utils.POWZN(gI, delta_m.ThenDiv(&sk.X)).ThenMul(&R.G_a)
+
+	return R_p
+}
---
+++ scheme/CH/CH_KEF_CZK_2004/PBC_test.go
@@ -0,0 +1,123 @@
+package CH_KEF_CZK_2004
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Hugu1e/ChameLibGo/curve"
+	"github.com/Nik-U/pbc"
+)
+
+func run_scheme(t *testing.T, cur curve.Curve, group pbc.Field){
+	pp := SetUp(cur, group)
+
+	pk, sk := KeyGen(pp)
+
+	m1 := pp.GetZrElement()
+	m2 := pp.GetZrElement()
+	if m1.Equals(m2) {
+		t.Errorf("m1 == m2")
+	}
+
+	// var L1 *pbc.Element
+	// var L2 *pbc.Element
+	// // these group has wrong behave in fromhash
+	// if group == pbc.GT && (cur == curve.D_159 || cur == curve.D_201 || cur == curve.D_224 || cur == curve.D_105171_196_185 || cur == curve.D_277699_175_167 || cur == curve.D_278027_190_181 || cur == curve.F || cur == curve.SM_9 || cur == curve.G_149) {
+	// 	L1 = pp.GetGroupElement();
+	// 	L2 = pp.GetGroupElement();
+	// } else {
+	// 	L1 = pp.H("S11|R11|T11");
+	// 	L2 = pp.H("S22|R22|T22");
+	// }
+
+	L1 := pp.GetGroupElement()
+	L2 := pp.GetGroupElement()
+	if L1.Equals(L2) {
+		t.Errorf("L1 == L2")
+	}
+
+	h1, r1 := Hash(pp, pk, L1, m1)
+	t.Log("h1.H", h1.H.String())
+	t.Log("r1.G_a", r1.G_a.String())
+	t.Log("r1.Y_a", r1.Y_a.String())
+	if !Check(h1, r1, pp, L1, m1) {
+		t.Errorf("H(L1, m1) invalid")
+	}
+	if Check(h1, r1, pp, L2, m1) {
+		t.Error()
+	}
+	
+	h2, r2 := Hash(pp, pk, L2, m2)
+	if !Check(h2, r2, pp, L2, m2) {
+		t.Errorf("H(L2, m2) invalid")
+	}
+	if Check(h2, r2, pp, L1, m2) {
+		t.Error()
+	}
+	
+	if Check(h1, r1, pp, L2, m2) {
+		t.Error()
+	}
+	if Check(h2, r2, pp, L1, m1) {
+		t.Error()
+	}
+	
+	r1_p := Adapt(r1, pp, sk, L1, m1, m2)
+	t.Log("r1_p.G_a", r1_p.G_a.String())
+	t.Log("r1_p.Y_a", r1_p.Y_a.String())
+	if !Check(h1, r1_p, pp, L1, m2) {
+		t.Errorf("Adapt(m2) invalid")
+	}
+	if Check(h2, r1_p, pp, L1, m1) {
+		t.Error()
+	}
+}
+
+func Test_PBC(t *testing.T) {
+	curs := []curve.Curve{
+		curve.A,
+		curve.A1,
+		curve.D_159,
+		curve.D_201,
+		curve.D_224,
+		curve.D_105171_196_185,
+		curve.D_277699_175_167,
+		curve.D_278027_190_181,
+		curve.E,
+		curve.F,
+		curve.SM_9,
+		curve.G_149,
+	}
+	groups := []pbc.Field{
+        pbc.G1,
+        pbc.G2,
+        pbc.GT,
+    }
+
+	cases := []struct {
+		cur curve.Curve
+		group pbc.Field
+	}{}
+
+	for _, c := range curs {
+		for _, g := range groups {
+            cases = append(cases, struct {
+                cur  curve.Curve
+                group pbc.Field
+            }{
+                cur:  c,
+                group:g,
+            })
+			
+		}
+	}
+
+	for i, c := range cases {
+		curveName := curve.CurveName[c.cur]
+        groupName := curve.GroupName[c.group]
+		t.Run(fmt.Sprintf("case %d %s %s", i+1, curveName, groupName), func(t *testing.T) {
+			run_scheme(t, c.cur, c.group)
+		})
+	}
+
+}
